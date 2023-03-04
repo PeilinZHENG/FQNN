eb540d227ee7662bbc0e0a8874f61504aac1fcbe
PeilinZHENG <phylincszheng@gmail.com>
2023-03-04 12:09:34 +0800

    Update

--- FK_Train.py
+++ FK_Train.py
@@ -293,7 +293,8 @@ def main_worker(args):
     val_dataset = LoadFKHamData(valdata, vallabels)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers,
                               pin_memory=True)
-    val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=args.workers, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers,
+                            pin_memory=True)
     print('trainset:{}\ttrainloader:{}\tvalset:{}\tvalloader:{}'.format(
         len(train_dataset), len(train_loader), len(val_dataset), len(val_loader)))
     args.f.write('trainset:{}\ttrainloader:{}\tvalset:{}\tvalloader:{}\n\n'.format(
