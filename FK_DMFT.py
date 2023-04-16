6821d712e72deb5fcaaa990871db441791535288
PeilinZHENG <phylincszheng@gmail.com>
2023-04-16 14:07:26 +0800

    Update

--- FK_DMFT.py
+++ FK_DMFT.py
@@ -288,8 +288,8 @@ if __name__ == "__main__":
 
     L = 12  # size = L ** 2
     data = f'FK_{L}'
-    Net = 'Naive_0'
-    T = 0.15
+    Net = 'Naive_2d_1'
+    T = 0.10
     save = True
     show = True
 
